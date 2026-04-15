1dada7154a464f0c886d8db91cf9216b74c994a5
ANTO <ant.castillos@duocuc.cl>
2026-04-15 14:24:46 -0400

    clase service terminada y lista

--- antonella/src/main/java/com/castillo/antonella/service/EventoService.java
+++ antonella/src/main/java/com/castillo/antonella/service/EventoService.java
@@ -1,5 +1,57 @@
 package com.castillo.antonella.service;
 
-public class EventoService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.castillo.antonella.model.Evento;
+import com.castillo.antonella.repository.EventoRepository;
+
+
+@Service
+public class EventoService 
+{
+    @Autowired
+    private EventoRepository eventoRepository;
+
+    public List<Evento> readAll()
+    {
+        return eventoRepository.obtenerEventos();
+
+    }
+
+    public Evento create(Evento evento)
+    {
+        return eventoRepository.guardarEvento(evento);
+
+    }
+
+    public Evento readById(int id)
+    {
+        return eventoRepository.buscarPorId(id);
+    }
+
+    public Evento update(int id, Evento evento)
+    {
+        return eventoRepository.actualizarEvento(id, evento);
+
+    }
+
+    public boolean delete(int id)
+    {
+        return eventoRepository.eliminarEvento(id);
+    }
+
+    public List<Evento> readByTipo(String tipoEvento)
+    {
+        return eventoRepository.flitrarEventosPorTipo(tipoEvento);
+
+    }
+
+    public List<Evento> orderByFecha()
+    {
+        return eventoRepository.ordenarPorFecha();
+    }
 
 }
