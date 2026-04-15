367e670c3b5008e8b3c2075fcf99052a7d2109c7
Anto <ant.castillos@duocuc.cl>
2026-04-15 11:58:39 -0400

    crear proyecto spring boot

---
+++ antonella/src/test/java/com/castillo/antonella/AntonellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castillo.antonella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntonellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
